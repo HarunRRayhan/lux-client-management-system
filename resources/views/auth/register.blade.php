7ac189792a682460882f2e385c6b26501fd91bc5
Harun R Rayhan <harun.b13@gmail.com>
2020-09-09 15:56:54 +0600

    update user register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,32 +1,67 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-jet-authentication-card-logo/>
         </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+        <x-jet-validation-errors class="mb-4"/>
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div>
-                <x-jet-label value="Name" />
-                <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <div class="flex justify-content-center mt-1">
+                <div>
+                    <x-jet-label value="First Name"/>
+                    <x-jet-input
+                        class="mr-2"
+                        type="text"
+                        name="first_name"
+                        :value="old('first_name')"
+                        required
+                        autofocus
+                        autocomplete="first_name"
+                    />
+                </div>
+
+                <div>
+                    <x-jet-label value="Last Name"/>
+                    <x-jet-input
+                        class=""
+                        type="text"
+                        name="last_name"
+                        :value="old('last_name')"
+                        required
+                        autofocus
+                        autocomplete="last_name"
+                    />
+                </div>
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="Email" />
-                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-label value="Email"/>
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required/>
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="Password" />
-                <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-label value="Password"/>
+                <x-jet-input
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="Confirm Password" />
-                <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-label value="Confirm Password"/>
+                <x-jet-input
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="flex items-center justify-end mt-4">
